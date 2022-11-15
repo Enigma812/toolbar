5b43b624e5c002e858bef36cb6b78deddc2e5b12
Enigma812 <antiseptic812@gmail.com>
2022-11-15 23:05:37 +0300

    feat: add start component

--- src/modules/account/account-routing.module.ts
+++ src/modules/account/account-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account-page/account.component';
 
-const routes: Routes = [{ path: '', component: AccountComponent }];
+const routes: Routes = [
+  {
+    path: '', component: AccountComponent
+  },
+  {
+    path: 'start', loadChildren: () => import('../start/start.module').then(m => m.StartModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
