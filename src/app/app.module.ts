54fdd30c9479100e965c9fe4addd4e8b15f78293
Enigma812 <antiseptic812@gmail.com>
2022-11-29 18:48:03 +0300

    build: project update angular material 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
